751755652bdf1de3607f0072df8e3179223ac681
GeeseThePriest <onegeese1433@gmail.com>
2026-05-03 22:22:30 +0800

    Created db file, sql file modified to create table

--- students.sql
+++ students.sql
@@ -1,6 +1,16 @@
 BEGIN TRANSACTION;
 DROP TABLE IF EXISTS "students";
 CREATE TABLE students (
-
+	id INTEGER PRIMARY KEY,
+	first_name TEXT NOT NULL,
+	middle_name TEXT NOT NULL, 
+	last_name TEXT NOT NULL, 
+	gender TEXT NOT NULL, 
+	birthdate TEXT NOT NULL,
+	place_of_birth TEXT NOT NULL,
+	email_address TEXT NOT NULL, 
+	contact_number TEXT NOT NULL, 
+	class_section TEXT NOT NULL, 
+	leauge_color TEXT NOT NULL 
 );
 COMMIT;
